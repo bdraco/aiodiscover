b9afdeb0fbe678fff77e57e5cb7a28512d8a8c29
J. Nick Koston <nick@koston.org>
2021-03-27 13:17:50 -1000

    Fix test

--- aiodiscover/tests/test_discovery.py
+++ aiodiscover/tests/test_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pytest
 
 from aiodiscover import DiscoverHosts
 
 
+@pytest.mark.asyncio
 async def test_async_discover_hosts():
     """Verify discover hosts does not throw."""
     discover_hosts = DiscoverHosts()
