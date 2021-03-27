0affef0458a584b9674b2fd6ca4444f5e1edbc63
J. Nick Koston <nick@koston.org>
2021-03-27 12:41:14 -1000

    Initial commit

---
+++ aiodiscover/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Async Host discovery."""
+
+__author__ = "J. Nick Koston"
+__email__ = "nick@koston.org"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "1.0.0"
+
+
+def get_module_version():
+    return __version__
+
+
+from .example import Example  # noqa: F401
