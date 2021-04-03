0a31b9dc95c0220f10539bd3dd274d9aa3228fac
J. Nick Koston <nick@koston.org>
2021-04-02 14:08:32 -1000

    Increase ptr resolver coverage

--- aiodiscover/tests/test_discovery.py
+++ aiodiscover/tests/test_discovery.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from unittest.mock import patch
+
 import pytest
 
-from unittest.mock import patch
 from aiodiscover import discovery
+
+UDP_PTR_RESOLUTION_OCTETS = (
+    b"\x8cV\x85\x80\x00\x01\x00\x01\x00\x00\x00\x00\x0249\x03107\x03168\x03192\x07"
+    b"in-addr\x04arpa\x00\x00\x0c\x00\x01\xc0\x0c\x00\x0c\x00\x01\x00\x00\x00\x00"
+    b"\x00(\x1bBroadlink_RMPROSUB-cc-ce-9f\x06koston\x03org\x00"
+)
 
 
 @pytest.mark.asyncio
@@ -48,3 +55,29 @@ async def test_async_discover_hosts_with_dns_mock_neighbor_mock():
         {"hostname": "router", "ip": "1.2.3.4", "macaddress": "aa:bb:cc:dd:ee:ff"},
         {"hostname": "any", "ip": "4.5.5.6", "macaddress": "ff:bb:cc:dd:ee:ff"},
     ]
+
+
+@pytest.mark.asyncio
+async def test_ptr_resolver_protocol_ignores_other_sources():
+    """Test that the PTRResolver ignores responses from unexpected sources."""
+    destination = ("192.168.107.1", discovery.DNS_PORT)
+    other_destination = ("192.168.107.2", discovery.DNS_PORT)
+    ptr_resolver = discovery.PTRResolver(destination)
+    ptr_resolver.datagram_received(UDP_PTR_RESOLUTION_OCTETS, destination)
+    assert 35926 in ptr_resolver.responses
+    ptr_resolver.responses = {}
+    ptr_resolver.datagram_received(UDP_PTR_RESOLUTION_OCTETS, other_destination)
+    assert 35926 not in ptr_resolver.responses
+
+
+@pytest.mark.asyncio
+async def test_ptr_resolver_can_parse():
+    """Test that the PTRResolver can parse a response."""
+    destination = ("192.168.107.1", discovery.DNS_PORT)
+    ptr_resolver = discovery.PTRResolver(destination)
+    ptr_resolver.datagram_received(UDP_PTR_RESOLUTION_OCTETS, destination)
+    assert 35926 in ptr_resolver.responses
+    assert (
+        discovery.dns_message_short_hostname(ptr_resolver.responses[35926])
+        == "broadlink_rmprosub-cc-ce-9f"
+    )
