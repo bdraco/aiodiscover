feb1ce846e3b6adaa39160e64006710c69a5e0ed
J. Nick Koston <nick@koston.org>
2022-04-17 16:25:23 -1000

    Bump version: 1.4.8 → 1.4.9

--- setup.py
+++ setup.py
@@ -86,6 +86,6 @@ setup(
     url="https://github.com/bdraco/aiodiscover",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.4.8",
+    version="1.4.9",
     zip_safe=False,
 )
