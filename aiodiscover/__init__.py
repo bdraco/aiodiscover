647b0f00a21305339898dc6cd2efb0d8e8d90a4a
J. Nick Koston <nick@koston.org>
2022-09-11 15:12:42 -0500

    Bump version: 1.4.11 → 1.4.12

--- aiodiscover/__init__.py
+++ aiodiscover/__init__.py
@@ -4,7 +4,7 @@ __author__ = "J. Nick Koston"
 __email__ = "nick@koston.org"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "1.4.11"
+__version__ = "1.4.12"
 
 from .discovery import DiscoverHosts  # noqa: F401
 
