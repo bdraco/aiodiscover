d5b7ceef5d39e0f8e10503f384302797a8343faa
J. Nick Koston <nick@koston.org>
2025-02-09 15:09:25 -0600

    chore: add tests for parsing resolv.conf

---
+++ aiodiscover/tests/test_network.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import asyncio
+import sys
+from ipaddress import IPv4Address, IPv6Address
+
+from aiodiscover.network import parse_resolv_conf
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+def test_parse_resolv_conf() -> None:
+    """Verify parse_resolv_conf."""
+    resolv_conf = parse_resolv_conf(
+        [
+            "# This is a comment",
+            "; This is a comment",
+            "  ; This is a comment",
+            "nameserver 3.3.4.3",
+            "   nameserver   32.2.1.1   ",
+            " nameserver        2001:4860:4860::8888",
+        ]
+    )
+    assert resolv_conf == [
+        IPv4Address("3.3.4.3"),
+        IPv4Address("32.2.1.1"),
+        IPv6Address("2001:4860:4860::8888"),
+    ]
